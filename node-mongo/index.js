7dd0cdd25ef39832fee9f4678325c6f8c8231fe2
UDAYADITYA SINGH <udayaditya.singh@gmail.com>
2019-08-05 12:11:46 +0530

    "Functional DB Operatios"

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 var mongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var dboper = require('./operations');
 
 var url = 'mongodb://localhost:27017';
 
@@ -8,8 +9,43 @@ mongoClient.connect(url, function (err, client)  {
     console.log('Connected to MongoDb Server');
 
     var db = client.db('conFusion');
-    var collection = db.collection('conFusion');
+    var collection = db.collection('dishes');
 
+    /**
+     * Functional DB Operations
+     */
+
+
+    dboper.insertDocument(
+        db,
+        {
+            "name": "Uthappizza", 
+            "description": "test"
+        },
+        "dishes",
+        function(result){
+            console.log("Inserted Documented : \n");
+            console.log(result.ops);
+            dboper.findDocuments(db, "dishes", function(docs){
+                console.log("Found Documents: \n", docs);
+                dboper.updateDocument(db,{name : "Uthappizza"}, {description: 'updated test'},"dishes", function(result){
+                    console.log("Updated Document: \n", result.result);
+                    dboper.findDocuments(db, "dishes", function(docs){
+                        console.log("Found updated Documents: \n", docs);
+                        db.dropCollection('dishes', function(result){
+                            console.log('Dropped Collcetion : \n', result);
+                            client.close();
+                        });
+                    });
+                });
+            });
+            
+        });
+
+
+/**
+ * Direction Calling DB Operations
+ */
     collection.insertOne({
         "name": "Uthappizza", 
         "description": "test"
