e7d2ef717cd106bde8d14f265570fec24dc600dd
UDAYADITYA SINGH <udayaditya.singh@gmail.com>
2019-08-05 13:11:09 +0530

    "Mongo with promises"

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,6 +4,18 @@ var dboper = require('./operations');
 
 var url = 'mongodb://localhost:27017';
 
+/**
+ * With Promises
+ */
+
+// MongoClient.connect(url)
+// .then(function (client) { // <- db as first argument
+//   console.log(client)
+// })
+// .catch(function (err) {
+
+// });
+
 mongoClient.connect(url, function (err, client)  {
     assert.equal(err, null);
     console.log('Connected to MongoDb Server');
@@ -46,25 +58,29 @@ mongoClient.connect(url, function (err, client)  {
 /**
  * Direction Calling DB Operations
  */
-    collection.insertOne({
-        "name": "Uthappizza", 
-        "description": "test"
-    }, function (err, result){
-        assert.equal(err, null);
-        console.log("After Insert: \n");
-        console.log(result.ops);
-        collection.find({}).toArray(function(err, docs){
-            assert.equal(err, null);
-            console.log("Found: \n");
-            console.log(docs);
-            if(docs){
-                db.dropCollection("conFusion", function(err, result){
-                    assert.equal(err, null);
+    // collection.insertOne({
+    //     "name": "Uthappizza", 
+    //     "description": "test"
+    // }, function (err, result){
+    //     assert.equal(err, null);
+    //     console.log("After Insert: \n");
+    //     console.log(result.ops);
+    //     collection.find({}).toArray(function(err, docs){
+    //         assert.equal(err, null);
+    //         console.log("Found: \n");
+    //         console.log(docs);
+    //         if(docs){
+    //             db.dropCollection("conFusion", function(err, result){
+    //                 assert.equal(err, null);
     
-                    client.close();
-                });
-            }
-        });
+    //                 client.close();
+    //             });
+    //         }
+    //     });
         
-    });
+    // });
+
+
+
+
 });
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -3,36 +3,78 @@ var assert = require('assert');
 exports.insertDocument = function(db, document, collection, callback){
     var coll = db.collection(collection);
 
+    /**
+     * With Callback
+     */
     coll.insert(document, function(err, result) {
         assert.equal(err, null);
         console.log('Result', result.result.n + "documents into the collection " + collection);
         callback(result);
     });
+
+    /**
+     * With Promise
+     */
+
+    //  return coll.insert(document);
 };
 
 exports.findDocuments = function(db, collection, callback){
     var coll = db.collection(collection);
+
+    /**
+     * With Callback
+     */
     coll.find({}).toArray(function(err, result) {
         assert.equal(err, null);
         callback(result);
     });
 
+    /**
+     * With Promises
+     */
+    // return coll.find({}).toArray();
+
 };
 
 exports.removeDocument = function(db, document, collection, callback){
     var coll = db.collection(collection);
+
+    /**
+     * With Callback
+     */
     coll.deleteOne(document, function(err, result){
         assert.equal(err, null);
         console.log('Removed the document', document);
         callback(result);
     });
+
+    /**
+     * With Promises
+     */
+    
+    // return  coll.deleteOne(document);
+
+
 };
 
 exports.updateDocument = function(db, document, update,  collection, callback){
     var coll = db.collection(collection);
-    coll.updateOne(document,{ $set: update},null,function(err, result){
+
+    /**
+     * With Callback 
+     */
+
+     coll.updateOne(document,{ $set: update},null,function(err, result){
         assert.equal(err, null);
         console.log('Updated the document', document);
         callback(result);
     });
+
+    /**
+     * With Promises
+     */
+
+    return coll.updateOne(document);
+    
 };
